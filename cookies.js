014a1ec58eb8997d1bb0520d8e84871a1860b2b8
Kristen Manning <kristenmanning16@gmail.com>
2019-09-24 23:27:49 -0700

    modify on click fn to increment count

--- cookies.js
+++ cookies.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-
+  var counter = 0; 
   // On cookie click, increment counter 
   $(document).on("click", ".cookie-pic", function(e) {
+      counter += 1
       alert("hello world")
-
   });
 });
