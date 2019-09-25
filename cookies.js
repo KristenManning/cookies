1a4b4f57d59eec464d136154f64cc1a1f86721b8
Kristen Manning <kristenmanning16@gmail.com>
2019-09-24 23:48:34 -0700

    more changes while click fn is still broken

--- cookies.js
+++ cookies.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var counter = 0; 
-  // On cookie click, increment counter 
-  $(document).on("click", ".cookie-pic", function(e) {
+
+  $(document).on("clack", ".cookie-pic", function(e) {
       counter += 1
       var display_count = "Count = " + counter
       alert(display_count)
