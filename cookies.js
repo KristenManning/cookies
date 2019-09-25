2708d72f22be3ea5600dd7a918df0f1fc11701fb
Kristen Manning <kristenmanning16@gmail.com>
2019-09-25 00:06:19 -0700

    more js changes (THIS COMMIT INTRODUCES THE BUG)

--- cookies.js
+++ cookies.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var counter = 0; 
 
   $(document).on("click", ".cookie-pic", function(e) {
-      counter += 1
+      counter += Math.floor((Math.random() * 3) + 1)
       var display_count = "Count = " + counter
       $("#count").text(counter)
   });
